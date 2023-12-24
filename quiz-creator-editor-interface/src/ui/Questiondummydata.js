df42c34285438f9d255117f3cbef16e2f826b532
yasmenmohamedsh@gmail.com <yasmenmohamedsh@gmail.com>
2023-12-24 14:25:43 +0200

    Set initial file structure

---
+++ quiz-creator-editor-interface/src/ui/Questiondummydata.js
@@ -0,0 +1,22 @@
+const questionsData = [
+    {
+      id: 1,
+      text: 'What is the capital of France?',
+      answers: [
+        { id: 1, text: 'Paris', isTrue: true },
+        { id: 2, text: 'Madrid', isTrue: false },
+        { id: 3, text: 'Berlin', isTrue: false },
+        { id: 4, text: 'Rome', isTrue: false },
+      ],
+    },
+    {
+      id: 2,
+      text: 'Which planet is known as the Red Planet?',
+      answers: [
+        { id: 5, text: 'Mars', isTrue: true },
+        { id: 6, text: 'Venus', isTrue: false },
+        { id: 7, text: 'Jupiter', isTrue: false },
+        { id: 8, text: 'Saturn', isTrue: false },
+      ],
+    },
+  ];
