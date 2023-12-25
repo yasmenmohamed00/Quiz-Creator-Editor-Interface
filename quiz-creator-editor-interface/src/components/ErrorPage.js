b60794499696526dc42d76452846f84a9f9aa412
yasmenmohamedsh@gmail.com <yasmenmohamedsh@gmail.com>
2023-12-25 19:52:27 +0200

    create ErrorPage

---
+++ quiz-creator-editor-interface/src/components/ErrorPage.js
@@ -0,0 +1,9 @@
+function ErrorPage(){
+    return(
+        <div className="alert alert-warning" role="alert">
+        This is a warning alert—check it out!
+        </div>
+    );
+}
+
+export default ErrorPage;
